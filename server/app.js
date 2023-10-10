9c1d5a91319dde39e85c45054e823ab26c0fc9ca
Nebojsa001 <ntrivunovic75@gmail.com>
2023-10-10 13:03:15 +0200

    Init

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const app = express();
 const userRouter = require("./routes/userRoutes");
 
 const corsOptions = {
-  origin: "http://127.0.0.1:5500",
+  origin: "https://arena-game-penalty.netlify.app",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: "Content-Type,Authorization",
 };
